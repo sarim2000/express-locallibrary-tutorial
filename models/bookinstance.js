9cc7683acd24240c1c1c20a7106a26649ad97940
sarim2000 <sarimbleedblue@gmail.com>
2022-01-13 01:48:44 +0530

    added exoress validator

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -20,7 +20,7 @@ BookInstanceSchema
 BookInstanceSchema
 .virtual('due_back_formatted')
 .get(function () {
-  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_FULL);
 });
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
